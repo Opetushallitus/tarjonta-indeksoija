558c92de9ecff386af8457c18a3af7610e090af3
Alexis Troberg <alexistroberg@gmail.com>
2016-09-29 10:40:37 +0300

    Enable indexing of hakukohde and haku

--- src/lib/tarjontaSearch.js
+++ src/lib/tarjontaSearch.js
@@ -2,13 +2,19 @@ import fetch from 'node-fetch'
 import _ from 'lodash'
 import config from '../config'
 
-const tarjontaSearch = (entity, queryString) =>
-  fetch(`${config.baseUrl}/${entity}/search?${queryString}`)
+const mapOid = tulokset => tulokset.map(t => t.oid)
+
+const tarjontaSearch = (entity, queryString) => {
+  if (entity === 'haku') {
+    return fetch(`${config.baseUrl}/${entity}/find?${queryString}`)
+      .then(response => response.json())
+      .then(response => mapOid(response.result))
+  }
+  return fetch(`${config.baseUrl}/${entity}/search?${queryString}`)
     .then(response => response.json())
     .then(response => response.result.tulokset)
-    .then(tuloksetByOrg => tuloksetByOrg.map(orgTulos =>
-      orgTulos.tulokset.map(t => t.oid)
-    ))
+    .then(tuloksetByOrg => tuloksetByOrg.map(orgTulos => mapOid(orgTulos.tulokset)))
     .then(_.flatten)
+}
 
 export default tarjontaSearch
