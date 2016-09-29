3199658e15ea401b715bbbd71b1ed19795421f94
Alexis Troberg <alexistroberg@gmail.com>
2016-09-29 11:48:36 +0300

    Add parallelism to indexing to increase performance

--- src/cli.js
+++ src/cli.js
@@ -1,14 +1,18 @@
-import eachSeries from 'async/eachSeries'
+import eachLimit from 'async/eachLimit'
 import ProgressBar from 'progress'
 import tarjontaSearch from './lib/tarjontaSearch'
 import pickIndexer from './lib/pickIndexer'
 
 const entity = process.argv[2]
 const queryString = process.argv[3]
+const parallellIndexingOperations = 6
 
 tarjontaSearch(entity, queryString).then((matchingOids) => {
+  console.log(`Indexing ${matchingOids.length} documents`)
+
   const progressBar = new ProgressBar(':bar', { total: matchingOids.length })
-  eachSeries(matchingOids, (oid, done) => {
+
+  eachLimit(matchingOids, parallellIndexingOperations, (oid, done) => {
     progressBar.tick()
     pickIndexer(entity)(oid)
       .then(() => done())
@@ -17,7 +21,7 @@ tarjontaSearch(entity, queryString).then((matchingOids) => {
     if (error) {
       console.error(error)
     } else {
-      console.log('All indexed')
+      console.log('All documents indexed')
     }
   })
 })
