a4ff333486aa89f361ff3900f83d0b7830f41357
Petteri Turtiainen <petteri.turtiainen@cybercom.com>
2017-01-16 10:52:01 +0200

    Clojure initial commit

---
+++ tarjonta-indeksoija-service/src/tarjonta_indeksoija_service/handler.clj
@@ -0,0 +1,19 @@
+(ns tarjonta-indeksoija-service.handler
+  (:require [compojure.api.sweet :refer :all]
+            [ring.util.http-response :refer :all]
+            [schema.core :as s]))
+
+(def app
+  (api
+    {:swagger
+     {:ui "/"
+      :spec "/swagger.json"
+      :data {:info {:title "Tarjonta-indeksoija-service"
+                    :description "Compojure Api example"}
+             :tags [{:name "api", :description "some apis"}]}}}
+
+    (context "/api" []
+      :tags ["api"]
+
+
+      )))
---
+++ tarjonta-indeksoija-service/test/tarjonta_indeksoija_service/core_test.clj
@@ -0,0 +1,8 @@
+(ns tarjonta-indeksoija-service.core-test
+  (:require [cheshire.core :as cheshire]
+            [midje.sweet :refer :all]
+            [tarjonta-indeksoija-service.handler :refer :all]
+            [ring.mock.request :as mock]))
+
+
+
