2416d2d0c9a7df8140c5b88fcbfb0d912edef10a
Petteri Turtiainen <petteri.turtiainen@cybercom.com>
2017-01-19 10:28:23 +0200

    Added build script for bamboo

---
+++ tarjonta-indeksoija-service/ci/build.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+export JAVA_HOME="${bamboo_capability_system_jdk_JDK_1_8}"
+export PATH=$JAVA_HOME/bin:$PATH
+
+test() {
+  ./ci/lein clean
+  ./ci/lein compile
+  ./ci/lein test
+}
+
+uberjar() {
+  ./ci/lein clean
+  echo ${bamboo_buildResultKey} > ./resources/build.txt
+  git rev-parse HEAD > ./resources/git-rev.txt
+  ./ci/lein uberjar
+}
+
+command="$1"
+
+case "$command" in
+    "test" )
+        test
+        ;;
+    "uberjar" )
+        uberjar
+        ;;
+    *)
+        echo "Unknown command $command"
+        ;;
+esac
