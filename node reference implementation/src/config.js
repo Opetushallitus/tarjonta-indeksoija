d8beae961fa6ec4bd90a63997a30d4299db37c75
Petteri Turtiainen <petteri.turtiainen@cybercom.com>
2017-01-25 10:24:01 +0200

    Removed node reference implementation.

--- node reference implementation/src/config.js
+++
@@ -1,6 +0,0 @@
-const config = {
-  baseUrl: 'https://testi.virkailija.opintopolku.fi/tarjonta-service/rest/v1',
-  elasticSearchLocation: 'localhost:9200',
-}
-
-export default config
--- node reference implementation/src/lib/esClient.js
+++
@@ -1,8 +0,0 @@
-import elasticsearch from 'elasticsearch'
-import config from '../config'
-
-const esClient = new elasticsearch.Client({
-  host: config.elasticSearchLocation,
-})
-
-export default esClient
