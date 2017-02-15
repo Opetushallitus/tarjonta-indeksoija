fa7639f3b2f1381c46d91805101482529ca58c21
Petteri Turtiainen <petteri.turtiainen@cybercom.com>
2017-02-15 10:45:17 +0200

    Exclude external api tests from bamboo build

--- test/mocks/index_mock.clj
+++
@@ -1,7 +0,0 @@
-(ns mocks.index-mock
-  (:require [tarjonta-indeksoija-service.elastic-client :as elastic-client]))
-
-(defn reindex-mock
-  [index params]
-  (let [docs {:type "hakukohde" :oid "1.2.246.562.20.28810946823"}]
-    (elastic-client/upsert-indexdata [docs])))
--- test/tarjonta_indeksoija_service/api_test.clj
+++ test/tarjonta_indeksoija_service/api_test.clj
@@ -3,27 +3,22 @@
             [tarjonta-indeksoija-service.elastic-client :as elastic-client]
             [tarjonta-indeksoija-service.test-tools :as tools :refer [parse-body reset-test-data]]
             [tarjonta-indeksoija-service.indexer :as indexer]
-            [mocks.tarjonta-mock :refer [get-doc with-tarjonta-mock]]
-            [mocks.index-mock :refer [reindex-mock]]
             [cheshire.core :as cheshire]
             [midje.sweet :refer :all]
             [ring.mock.request :as mock]))
 
-(facts "Api should"
+(facts "Api should" :external-api
   (against-background [(after :contents (reset-test-data))]
     (fact "reindex hakukohde"
       ;; This test uses tarjonta QA
       ;; TODO: try to mock tarjonta in this test..
-      (with-tarjonta-mock
-        (with-redefs [tarjonta-indeksoija-service.api/reindex reindex-mock]
-          (indexer/start-indexer-job)
-          (let [response (app (mock/request :get  "/tarjonta-indeksoija/api/reindex/hakukohde?hakukohdeOid=1.2.246.562.20.28810946823"))
-                body     (parse-body (:body response))]
-            (:status response) => 200)))
+      (indexer/start-indexer-job)
+      (let [response (app (mock/request :get  "/tarjonta-indeksoija/api/reindex/hakukohde?hakukohdeOid=1.2.246.562.20.28810946823"))
+            body     (parse-body (:body response))]
+        (:status response) => 200)
       (tools/block-until-indexed 10000)
       (elastic-client/get-queue) => [])
 
-    (tools/refresh-and-wait "hakukohde" 2000)
     (fact "fetch hakukohde"
       ;; uses result from previous test.
       (let [response (app (mock/request :get  "/tarjonta-indeksoija/api/august/hakukohde?oid=1.2.246.562.20.28810946823"))
@@ -33,17 +28,16 @@
     (fact "fetch koulutus tulos"
       ;; This test uses tarjonta QA and organisaatio
       (elastic-client/delete-index "hakukohde")
-      (with-tarjonta-mock
-        (elastic-client/upsert-indexdata [{:type "koulutus" :oid "1.2.246.562.17.53874141319"}
-                                          {:type "hakukohde" :oid "1.2.246.562.20.67506762722"}
-                                          {:type "hakukohde" :oid "1.2.246.562.20.715691882710"}
-                                          {:type "hakukohde" :oid "1.2.246.562.20.82790530479"}
-                                          {:type "hakukohde" :oid "1.2.246.562.20.17663370199"}
-                                          {:type "haku" :oid "1.2.246.562.29.86197271827"}
-                                          {:type "haku" :oid "1.2.246.562.29.59856749474"}
-                                          {:type "haku" :oid "1.2.246.562.29.53522498558"}
-                                          {:type "organisaatio" :oid "1.2.246.562.10.39920288212"}])
-        (tools/block-until-indexed 10000))
+      (elastic-client/upsert-indexdata [{:type "koulutus" :oid "1.2.246.562.17.53874141319"}
+                                        {:type "hakukohde" :oid "1.2.246.562.20.67506762722"}
+                                        {:type "hakukohde" :oid "1.2.246.562.20.715691882710"}
+                                        {:type "hakukohde" :oid "1.2.246.562.20.82790530479"}
+                                        {:type "hakukohde" :oid "1.2.246.562.20.17663370199"}
+                                        {:type "haku" :oid "1.2.246.562.29.86197271827"}
+                                        {:type "haku" :oid "1.2.246.562.29.59856749474"}
+                                        {:type "haku" :oid "1.2.246.562.29.53522498558"}
+                                        {:type "organisaatio" :oid "1.2.246.562.10.39920288212"}])
+      (tools/block-until-indexed 10000)
 
       (tools/refresh-and-wait "hakukohde" 0)
       (tools/refresh-and-wait "haku" 0)
