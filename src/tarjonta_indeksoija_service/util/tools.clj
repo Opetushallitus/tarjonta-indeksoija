bdccf240e7db6015e0af6ceb12dc887d4fa17c8b
Heikki Honkanen <heikki.honkanen@gofore.com>
2017-02-17 08:59:41 +0200

    Improve with-error-logging.

--- src/tarjonta_indeksoija_service/util/tools.clj
+++ src/tarjonta_indeksoija_service/util/tools.clj
@@ -1,17 +1,20 @@
 (ns tarjonta-indeksoija-service.util.tools
   (require [taoensso.timbre :as log]
-           [environ.core :as env]))
+           [environ.core :as env]
+           [slingshot.slingshot :refer [try+]]))
 
 (defmacro with-error-logging-value
   [value & body]
-  `(try
+  `(try+
      (do ~@body)
-     (catch Exception ~'e
+     (catch [:status 500] {:keys [~'trace-redirects]}
+       (log/error "HTTP 500 from:" ~'trace-redirects))
+     (catch Object ~'_
        (if (Boolean/valueOf (:test ~environ.core/env))
-         (log/info "Error during test:" (.getMessage ~'e))
+         (log/info "Error during test:" (:message ~'&throw-context))
 ;         (log/error ~'e)
          ; ^- during test if you want to see stack trace
-         (log/error ~'e))
+         (log/error (:throwable ~'&throw-context)))
        ~value)))
 
 (defmacro with-error-logging
