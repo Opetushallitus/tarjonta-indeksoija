47791677360e2bcea7b0815107afcfa154ee9f90
Jessika Penttinen <jessika.penttinen@siili.com>
2018-08-31 14:54:18 +0300

    Lisätty muuttuneiden eperusteiden ja organisaatioiden päivitys jobiin

--- src/konfo_indeksoija_service/indexer/job.clj
+++ src/konfo_indeksoija_service/indexer/job.clj
@@ -2,6 +2,8 @@
   (:require [konfo-indeksoija-service.indexer.index :refer [do-index]]
             [konfo-indeksoija-service.util.conf :refer [env job-pool]]
             [konfo-indeksoija-service.rest.tarjonta :as tarjonta]
+            [konfo-indeksoija-service.rest.organisaatio :as organisaatio]
+            [konfo-indeksoija-service.rest.eperuste :as eperuste]
             [konfo-indeksoija-service.elastic.queue :refer [set-last-index-time get-last-index-time upsert-to-queue]]
             [clj-log.error-log :refer [with-error-logging]]
             [konfo-indeksoija-service.util.logging :refer [to-date-string]]
@@ -22,22 +24,24 @@
        (do ~@body)
        (finally (reset! elastic-lock? false)))))
 
-(defjob indexing-job
-        [ctx]
-        (with-error-logging
-         (wait-for-elastic-lock
-          (let [now (System/currentTimeMillis)
-                last-modified (get-last-index-time)
-                changes-since (tarjonta/get-last-modified last-modified)]
-            (when-not (nil? changes-since)
-              (log/info "Fetched last-modified since" (to-date-string last-modified)", containing" (count changes-since) "changes.")
-              (let [related-koulutus (flatten (pmap tarjonta/get-related-koulutus changes-since))
-                    last-modified-with-related-koulutus (clojure.set/union changes-since related-koulutus)]
-                (if-not (empty? related-koulutus)
-                  (log/info "Fetched" (count related-koulutus) "related koulutukses for previous changes"))
-                (upsert-to-queue last-modified-with-related-koulutus)
-                (set-last-index-time now)
-                (do-index)))))))
+(defjob indexing-job [ctx]
+  (with-error-logging
+   (wait-for-elastic-lock
+    (let [now (System/currentTimeMillis)
+          last-modified (get-last-index-time)
+          tarjonta-changes (tarjonta/get-last-modified last-modified)
+          organisaatio-changes (organisaatio/find-last-changes last-modified)
+          eperuste-changes (eperuste/find-docs last-modified)
+          changes-since (clojure.set/union tarjonta-changes organisaatio-changes eperuste-changes)]
+      (when-not (nil? changes-since)
+        (log/info "Fetched last-modified since" (to-date-string last-modified)", containing" (count changes-since) "changes.")
+        (let [related-koulutus (flatten (pmap tarjonta/get-related-koulutus changes-since))
+              last-modified-with-related-koulutus (clojure.set/union changes-since related-koulutus)]
+          (if-not (empty? related-koulutus)
+            (log/info "Fetched" (count related-koulutus) "related koulutukses for previous changes"))
+          (upsert-to-queue last-modified-with-related-koulutus)
+          (set-last-index-time now)
+          (do-index)))))))
 
 (defn start-indexer-job
   ([] (start-indexer-job (:cron-string env)))
