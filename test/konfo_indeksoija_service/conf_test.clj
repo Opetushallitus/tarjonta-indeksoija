b11ceb1e824ebdd4cec84b70362c44736275ae17
Joona Haavisto <joona.haavisto@gofore.com>
2018-10-01 15:33:57 +0300

    Testien CI-korjauksia

--- test/konfo_indeksoija_service/conf_test.clj
+++ test/konfo_indeksoija_service/conf_test.clj
@@ -1,7 +1,8 @@
 (ns konfo-indeksoija-service.conf-test
-  (:require [midje.sweet :refer :all]))
+  (:require [midje.sweet :refer :all]
+            [konfo-indeksoija-service.util.conf :refer [env]]))
 
 (fact "Test that oph-configuration/config.edn.template and dev-resources/config.edn contain the same keys"
   (let [template-conf (read-string (slurp "./oph-configuration/config.edn.template"))
-        dev-conf (read-string (slurp "./dev_resources/config.edn.template"))]
+        dev-conf (read-string (slurp (if (= (:CI env) "true") "./ci/config.edn" "./dev_resources/config.edn")))]
     (keys dev-conf) => (keys template-conf)))
