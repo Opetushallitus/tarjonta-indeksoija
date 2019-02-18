4bfbac053c27564df2d108432805c09c81dcbeed
jarikujansuu <jari.kujansuu@gofore.com>
2019-02-18 11:46:15 +0200

    Väliaikaisesti kirjoitetaan tilamuutokset lokiin.

--- src/konfo_indeksoija_service/queue/state.clj
+++ src/konfo_indeksoija_service/queue/state.clj
@@ -1,8 +1,9 @@
 (ns konfo-indeksoija-service.queue.state)
 
 (defn set-state!
-  [state, message])
+  [state, message]
   ;; TODO Mark in db: timestamp, status = <status>
+  (println (str "set-state! " state " " message)))
 
 
 (defn set-states! [state messages] (doseq [msg messages] (set-state! state msg)))
