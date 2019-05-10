02fb9ec9e0f9d0c87cf2fcf8bc187b22631d993b
Jessika Penttinen <jessika.penttinen@siili.com>
2019-05-10 09:30:38 +0300

    Oph:n oid caller-id:hen ja poistettu clientSubSystemCode

--- src/kouta_indeksoija_service/rest/util.clj
+++ src/kouta_indeksoija_service/rest/util.clj
@@ -2,12 +2,9 @@
   (:require [clj-http.client :as client]
             [clojure.tools.logging :as log]))
 
-;Adds Caller-Id and clientSubSystemCode to header
 (defn add-callerinfo [options]
   (update-in options [:headers] assoc
-             "Caller-Id" "fi.opintopolku.kouta-indeksoija"
-             "clientSubSystemCode" "fi.opintopolku.kouta-indeksoija")
-  )
+             "Caller-Id" "1.2.246.562.10.00000000001.kouta-indeksoija"))
 
 (defn get [url opts]
   (let [options (add-callerinfo opts)]
