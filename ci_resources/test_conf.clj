2a720d76987235b85f94a44cb30097bc40f56913
Jessika Penttinen <jessika.penttinen@siili.com>
2019-05-28 13:47:53 +0300

    Poistettu turhat tiedostot

--- ci_resources/test_conf.clj
+++
@@ -1,7 +0,0 @@
-(require '[mount.core :as mount])
-
-(mount/start)
-
-(change-defaults :emitter 'midje.emission.plugins.junit
-                 :print-level :print-facts
-                 :colorize false)
