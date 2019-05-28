22b2be24871e320044e132d362508c8d93f0975e
Jessika Penttinen <jessika.penttinen@siili.com>
2019-05-28 15:35:14 +0300

    Päivitetty eastwood ja cloverage

--- project.clj
+++ project.clj
@@ -63,10 +63,10 @@
                                   [org.clojure/tools.namespace "0.2.11"]
                                   [criterium "0.4.4"]]
                    :plugins [[lein-ring "0.10.0"]
-                             [jonase/eastwood "0.2.3"]
+                             [jonase/eastwood "0.3.5"]
                              [lein-kibit "0.1.3" :exclusions [org.clojure/clojure]]
                              [lein-environ "1.1.0"]
-                             [lein-cloverage "1.0.9" :exclusions [org.clojure/clojure]]]
+                             [lein-cloverage "1.1.1" :exclusions [org.clojure/clojure]]]
                    :resource-paths ["dev_resources"]
                    :env {:dev "true"}
                    :ring {:reload-paths ["src"]}
@@ -101,9 +101,8 @@
             "deploy" ["with-profile" "+jar-with-test-fixture" "deploy"]
             "install" ["with-profile" "+jar-with-test-fixture" "install"]
             "ci-test" ["with-profile" "+ci-test" "test"]
-            ;"autotest" ["with-profile" "+test" "midje" ":autotest"]
-            ;"eastwood" ["with-profile" "+test" "eastwood"]
-            ;"cloverage" ["with-profile" "+test" "cloverage" "--runner" ":midje"]
+            "eastwood" ["eastwood" "{:test-paths []}"]
+            "cloverage" ["with-profile" "+test" "cloverage"]
             "uberjar" ["do" "clean" ["ring" "uberjar"]]
             "testjar" ["with-profile" "+jar-with-test-fixture" "jar"]}
   :jvm-opts ["-Dlog4j.configurationFile=dev_resources/log4j2.properties"])
