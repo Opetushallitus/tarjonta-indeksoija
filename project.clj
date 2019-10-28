61084639eddd28d5469610642d9a8ede7f99fd13
Jessika Penttinen <jessika.penttinen@siili.com>
2019-10-28 13:44:42 +0200

    Vaihdettu versionumeroa

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
 (cemerick.pomegranate.aether/register-wagon-factory!
   "http" #(org.apache.maven.wagon.providers.http.HttpWagon.))
 
-(defproject kouta-indeksoija-service "0.1.9-SNAPSHOT-KTO-245-v2"
+(defproject kouta-indeksoija-service "0.1.9-KTO-245-v2-SNAPSHOT"
   :description "FIXME: write description"
   :repositories [["releases" {:url "https://artifactory.opintopolku.fi/artifactory/oph-sade-release-local"
                               :username :env/artifactory_username
