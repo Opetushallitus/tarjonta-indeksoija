9adf1e9dacc69e10fd1ad1c73ffee479279c2e7d
Antti Sarjanoja <antti.sarjanoja@gofore.com>
2021-05-25 15:57:46 +0300

    version up

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
 (cemerick.pomegranate.aether/register-wagon-factory!
   "http" #(org.apache.maven.wagon.providers.http.HttpWagon.))
 
-(defproject kouta-indeksoija-service "7.8.0-SNAPSHOT"
+(defproject kouta-indeksoija-service "8.0.0-SNAPSHOT"
   :description "Kouta-indeksoija"
   :repositories [["releases" {:url "https://artifactory.opintopolku.fi/artifactory/oph-sade-release-local"
                               :username :env/artifactory_username
