91d8854b64bc8e8e3ea5d14836f6705148175141
Vesa Ruuskanen <vesa.ruuskanen@gofore.com>
2021-05-26 08:45:02 +0300

    KTO-1167: Yhteishaku koodiuri koodisto-tiedostosta

--- src/kouta_indeksoija_service/indexer/tools/hakutieto.clj
+++ src/kouta_indeksoija_service/indexer/tools/hakutieto.clj
@@ -1,20 +1,18 @@
 (ns kouta-indeksoija-service.indexer.tools.hakutieto
   (:require [kouta-indeksoija-service.indexer.tools.tyyppi :refer [remove-uri-version]]
+            [kouta-indeksoija-service.indexer.tools.koodisto :refer [koodiuri-yhteishaku-hakutapa]]
             [kouta-indeksoija-service.indexer.tools.search :refer [pohjakoulutusvaatimus-koodi-urit]]))
 
 (defn- kaytetaanHaunAikatauluaHakukohteessa?
   [hakukohde]
   (true? (:kaytetaanHaunAikataulua hakukohde)))
 
-; TODO This from koodisto file
-(defonce koodi-uri-yhteishaku "hakutapa_01")
-
 (defn- get-hakukohde-hakutieto
   [hakukohde haku]
   (-> {}
       (assoc :hakuajat (:hakuajat (if kaytetaanHaunAikatauluaHakukohteessa? haku hakukohde)))
       (assoc :hakutapa (:hakutapaKoodiUri haku))
-      (assoc :yhteishakuOid (when (= koodi-uri-yhteishaku (remove-uri-version (:hakutapaKoodiUri haku))) (:hakuOid haku)))
+      (assoc :yhteishakuOid (when (= koodiuri-yhteishaku-hakutapa (remove-uri-version (:hakutapaKoodiUri haku))) (:hakuOid haku)))
       (assoc :pohjakoulutusvaatimukset (pohjakoulutusvaatimus-koodi-urit hakukohde))
       (assoc :valintatavat (:valintatapaKoodiUrit hakukohde))))
 
@@ -23,5 +21,4 @@
   (map #(get-hakukohde-hakutieto % haku) (:hakukohteet haku)))
 
 (defn get-search-hakutiedot
-  [hakutieto]
-  (vec (flatten (map map-haut (:haut hakutieto)))))
+  [hakutieto]  (vec (flatten (map map-haut (:haut hakutieto)))))
