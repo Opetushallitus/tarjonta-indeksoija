4a3ad3ad3fbd3296e765bc968813bea1154bfccb
Risto Salama <risto.salama@gofore.com>
2021-09-21 12:26:29 +0300

    Add spec for hakukohde koulutustyyppikoodi indexing

--- test/kouta_indeksoija_service/fixture/external_services.clj
+++ test/kouta_indeksoija_service/fixture/external_services.clj
@@ -32,9 +32,11 @@
 
 (defn mock-alakoodit
   [koodi-uri alakoodi-uri]
-  (vector
-   {:koodiUri (str alakoodi-uri "_01") :nimi {:fi (str alakoodi-uri "_01" " nimi fi") :sv (str alakoodi-uri "_01" " nimi sv")}}
-   {:koodiUri (str alakoodi-uri "_02") :nimi {:fi (str alakoodi-uri "_02" " nimi fi") :sv (str alakoodi-uri "_02" " nimi sv")}}))
+  (if (= "koulutustyyppiabc" koodi-uri)
+    (vector {:koodiUri (str koodi-uri "_01") :nimi {:fi (str koodi-uri "_01" " nimi fi") :sv (str koodi-uri "_01" " nimi sv")}})
+    (vector
+     {:koodiUri (str alakoodi-uri "_01") :nimi {:fi (str alakoodi-uri "_01" " nimi fi") :sv (str alakoodi-uri "_01" " nimi sv")}}
+     {:koodiUri (str alakoodi-uri "_02") :nimi {:fi (str alakoodi-uri "_02" " nimi fi") :sv (str alakoodi-uri "_02" " nimi sv")}})))
 
 (defn mock-get-henkilo-nimi-with-cache
   [oid]
