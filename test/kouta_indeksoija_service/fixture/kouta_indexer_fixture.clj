de48b232b84c6497306e89201c7e0c6607e2457a
Risto Salama <risto.salama@gofore.com>
2021-10-22 11:33:26 +0300

    Handle koulutustyyppikoodi for telma, tuva and vapaa sivistys

--- test/kouta_indeksoija_service/fixture/kouta_indexer_fixture.clj
+++ test/kouta_indeksoija_service/fixture/kouta_indexer_fixture.clj
@@ -312,6 +312,10 @@
   [hakutieto]
   ["pohjakoulutusvaatimuskonfo_am"])
 
+(defn mock-koulutustyyppi-koodisto
+  [koodisto]
+  {:koodit [{:koodiUri "koulutustyyppiabc_01"}]})
+
 (defn toimipiste-children
   [oids]
   (map #(-> {}
@@ -440,7 +444,10 @@
                  kouta-indeksoija-service.fixture.kouta-indexer-fixture/mock-pohjakoulutusvaatimus-koodi-urit
 
                  kouta-indeksoija-service.rest.organisaatio/get-hierarkia-v4
-                 mock-organisaatio-hierarkia-v4]
+                 mock-organisaatio-hierarkia-v4
+                 
+                 kouta-indeksoija-service.indexer.koodisto.koodisto/get-from-index
+                 mock-koulutustyyppi-koodisto]
      (do ~@body)))
 
 (defn index-oppilaitokset
