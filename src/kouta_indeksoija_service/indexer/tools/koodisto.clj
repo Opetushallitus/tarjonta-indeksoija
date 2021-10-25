297d9c78baff8a73ac220790d92d503135644de8
Risto Salama <risto.salama@gofore.com>
2021-10-25 11:50:42 +0300

    Fix ei-harkinnanvaraisuutta logic

--- src/kouta_indeksoija_service/indexer/tools/koodisto.clj
+++ src/kouta_indeksoija_service/indexer/tools/koodisto.clj
@@ -54,7 +54,8 @@
 
 (defn ei-harkinnanvaraisuutta
   [koulutusKoodiUri]
-  (get-alakoodi-nimi-with-cache koulutusKoodiUri "hakulomakkeenasetukset_eiharkinnanvaraisuutta"))
+  (let [asetukset (list-alakoodi-nimet-with-cache koulutusKoodiUri "hakulomakkeenasetukset")]
+    (some #(= "hakulomakkeenasetukset_eiharkinnanvaraisuutta" (get % :koodiUri)) asetukset)))
 
 (defn pohjakoulutusvaatimuskonfo
   []
