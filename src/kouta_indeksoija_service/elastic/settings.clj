3b105411bbba0e9d7af1f6bfc9e7c4caed2b3d8c
August Kilponen <august.kilponen@gmail.com>
2021-12-22 13:13:24 +0200

    Lisätty decompound-kentille search analyzer

--- src/kouta_indeksoija_service/elastic/settings.clj
+++ src/kouta_indeksoija_service/elastic/settings.clj
@@ -217,8 +217,9 @@
                                        :search_analyzer "finnish_lemmatizer"
                                        :norms false
                                        :fields {:keyword { :type "keyword" :ignore_above 256}
-                                                :decompound {:type "text" :analyzer "finnish_lemmatizer_with_decompound"}
-                                                :words { :type "text" :analyzer "finnish_words"}}}}}
+                                                :decompound {:type "text"
+                                                             :analyzer "finnish_lemmatizer_with_decompound"
+                                                             :search_analyzer "finnish_lemmatizer"}}}}}
                        {:sv {:match "sv"
                              :match_mapping_type "string"
                              :mapping {:type "text"
@@ -226,8 +227,9 @@
                                        :search_analyzer "swedish_hunspell"
                                        :norms false
                                        :fields {:keyword { :type "keyword" :ignore_above 256}
-                                                :decompound {:type "text" :analyzer "swedish_hunspell_with_decompound"}
-                                                :words { :type "text" :analyzer "swedish_words"}}}}}
+                                                :decompound {:type "text"
+                                                             :analyzer "swedish_hunspell_with_decompound"
+                                                             :search_analyzer "swedish_hunspell"}}}}}
                        {:en {:match "en"
                              :match_mapping_type "string"
                              :mapping {:type "text"
