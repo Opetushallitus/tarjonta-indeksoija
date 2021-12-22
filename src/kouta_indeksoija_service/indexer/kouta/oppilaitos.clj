fbe3e71eaf4d62e37b43d08adb08ac85e14c8162
Heidi Lammi-Mihaljov <heidi.lammi-mihaljov@gofore.com>
2021-12-22 17:52:22 +0200

    KTO-1355: Haetaan yhteystiedot organisaatiopalvelusta ja lisätään oppilaitokselle

--- src/kouta_indeksoija_service/indexer/kouta/oppilaitos.clj
+++ src/kouta_indeksoija_service/indexer/kouta/oppilaitos.clj
@@ -40,20 +40,63 @@
     (seq oppilaitoksen-osa) (assoc :oppilaitoksenOsa (-> oppilaitoksen-osa
                                                          (common/complete-entry)
                                                          (dissoc :oppilaitosOid :oid)))))
+(defn create-kielistetty-yhteystieto
+  [yhteystieto yhteystieto-name languages]
+  (into {} (for [lang languages
+                 :let [result (yhteystieto-name
+                                (first
+                                  (filter
+                                    (fn [yhteystieto-map]
+                                      (re-find (re-pattern lang) (:kieli yhteystieto-map)))
+                                    yhteystieto)))]
+                 :when (not (nil? result))]
+             [(keyword lang) result])))
+
+(defn create-kielistetty-osoitetieto
+  [osoitetieto languages]
+  {:osoite (create-kielistetty-yhteystieto osoitetieto :osoite languages)
+   :postinumeroKoodiUri (:postinumeroUri (first (filter (fn [os] (get-in os [:postinumeroUri])) osoitetieto)))})
+
+(defn parse-yhteystiedot
+  [response languages]
+  (let [yhteystiedot (:yhteystiedot response)
+        sahkopostit (filter (fn [yhteystieto] (get-in yhteystieto [:email])) yhteystiedot)
+        puhelinnumerot (filter (fn [yhteystieto] (= "puhelin" (get-in yhteystieto [:tyyppi]))) yhteystiedot)
+        postiosoitteet (filter (fn [yhteystieto] (= "posti" (get-in yhteystieto [:osoiteTyyppi]))) yhteystiedot)
+        kayntiosoitteet (filter (fn [yhteystieto] (= "kaynti" (get-in yhteystieto [:osoiteTyyppi]))) yhteystiedot)]
+  [{:nimi (:nimi response)
+   :sahkoposti (create-kielistetty-yhteystieto sahkopostit :email languages)
+   :puhelinnumero (create-kielistetty-yhteystieto puhelinnumerot :numero languages)
+   :postiosoite (create-kielistetty-osoitetieto postiosoitteet languages)
+   :kayntiosoite (create-kielistetty-osoitetieto kayntiosoitteet languages)
+   }]))
 
 (defn- add-data-from-organisaatio-palvelu
   [organisaatio]
-  (let [org-from-organisaatio-palvelu (organisaatio-client/get-by-oid-cached (:oid organisaatio))]
-    (assoc organisaatio :status (:status org-from-organisaatio-palvelu))))
+  (let [org-from-organisaatio-palvelu (organisaatio-client/get-by-oid-cached (:oid organisaatio))
+       yhteystiedot (parse-yhteystiedot org-from-organisaatio-palvelu ["fi", "sv", "en"])]
+    (-> organisaatio
+        (assoc :status (:status org-from-organisaatio-palvelu))
+        (assoc-in [:metadata :yhteystiedot] yhteystiedot))
+    ))
 
 (defn- oppilaitos-entry-with-osat
   [organisaatio]
   (let [oppilaitos-oid (:oid organisaatio)
         oppilaitos (or (kouta-backend/get-oppilaitos oppilaitos-oid) {})
+        oppilaitos-from-organisaatiopalvelu (organisaatio-client/get-by-oid-cached oppilaitos-oid)
+        oppilaitos-languages (distinct
+                               (for [yhteystieto (get-in oppilaitos-from-organisaatiopalvelu [:yhteystiedot])
+                                     :let [language (get-in yhteystieto [:kieli])]]
+                                 (let [[_ lang] (re-find #"_(.+)#" language)] lang)))
+        yhteystiedot (parse-yhteystiedot oppilaitos-from-organisaatiopalvelu oppilaitos-languages)
+        oppilaitos-metadata (assoc (get-in oppilaitos [:metadata]) :yhteystiedot yhteystiedot)
+        enriched-oppilaitos (assoc oppilaitos :metadata oppilaitos-metadata)
         oppilaitoksen-osat (map #(add-data-from-organisaatio-palvelu %) (kouta-backend/get-oppilaitoksen-osat oppilaitos-oid))
         koulutukset (kouta-backend/get-koulutukset-by-tarjoaja (:oid organisaatio))
-        find-oppilaitoksen-osa (fn [child] (or (first (filter #(= (:oid %) (:oid child)) oppilaitoksen-osat)) {}))]
-    (-> (oppilaitos-entry organisaatio oppilaitos koulutukset)
+        find-oppilaitoksen-osa (fn [child] (or (first (filter #(= (:oid %) (:oid child)) oppilaitoksen-osat)) {}))
+        ]
+    (-> (oppilaitos-entry organisaatio enriched-oppilaitos koulutukset)
         (assoc :osat (->> (organisaatio-tool/get-indexable-children organisaatio)
                           (map #(oppilaitoksen-osa-entry % (find-oppilaitoksen-osa %)))
                           (vec)))
---
+++ test/kouta_indeksoija_service/indexer/kouta_oppilaitos_test.clj
@@ -0,0 +1,119 @@
+(ns kouta-indeksoija-service.indexer.kouta-oppilaitos-test
+  (:require [clojure.test :refer [deftest testing is]]
+            [kouta-indeksoija-service.indexer.kouta.oppilaitos :as oppilaitos]))
+
+(def oppilaitos-response
+  {:nimi
+   {:fi "Aalto-yliopisto"
+    :sv "Aalto-universitetet"
+    :en "Aalto University"}
+   :yhteystiedot
+   [{:osoiteTyyppi "kaynti"
+     :kieli "kieli_fi#1"
+     :postinumeroUri "posti_02150"
+     :postitoimipaikka "ESPOO"
+     :osoite "Otakaari 1"}
+    {:osoiteTyyppi "posti"
+     :kieli "kieli_fi#1"
+     :postinumeroUri "posti_00076"
+     :postitoimipaikka "AALTO"
+     :osoite "PL 11110"}
+    {:kieli "kieli_fi#1"
+     :email "hakijapalvelut@aalto.fi"}
+    {:kieli "kieli_fi#1"
+     :www "http://www.aalto.fi/studies"}
+    {:kieli "kieli_fi#1"
+     :numero "0294429290"
+     :tyyppi "puhelin"}
+    {:osoiteTyyppi "kaynti"
+     :kieli "kieli_sv#1"
+     :postinumeroUri "posti_02150"
+     :postitoimipaikka "ESBO"
+     :osoite "Otsvängen 1"}
+    {:osoiteTyyppi "posti"
+     :kieli "kieli_sv#1"
+     :postinumeroUri "posti_00076"
+     :postitoimipaikka "AALTO"
+     :osoite "PB 11110"}
+    {:kieli "kieli_sv#1"
+     :email "ansokningsservice@aalto.fi"}
+    {:kieli "kieli_sv#1"
+     :www "http://www.aalto.fi/sv/studies"}
+    {:kieli "kieli_sv#1"
+     :numero "0294429290"
+     :tyyppi "puhelin"}
+    {:kieli "kieli_en#1"
+     :email "admissions@aalto.fi"}
+    {:kieli "kieli_en#1"
+     :www "http://www.aalto.fi/en/studies"}
+    {:kieli "kieli_en#1"
+     :numero "+358294429290"
+     :tyyppi "puhelin"}]})
+
+(def languages ["fi", "sv", "en"])
+
+(deftest create-kielistetty-yhteystieto
+  (testing "returns empty map if empty list given as a parameter"
+    (is (= {}
+           (oppilaitos/create-kielistetty-yhteystieto {} :email languages))))
+
+  (testing "returns kielistetty sahkoposti with english as the only language"
+    (let [sahkoposti [{:kieli "kieli_en#1" :email "admissions@aalto.fi"}]]
+      (is (= {:en "admissions@aalto.fi"}
+             (oppilaitos/create-kielistetty-yhteystieto sahkoposti :email languages)))))
+  )
+
+(deftest create-kielistetty-osoitetieto
+  (testing "returns kielistetty osoitetieto with (katu)osoite and postinumero that has fields for name and koodiUri"
+    (let [postiosoite [{:osoiteTyyppi "posti"
+                        :kieli "kieli_fi#1"
+                        :postinumeroUri "posti_00076"
+                        :postitoimipaikka "AALTO"
+                        :osoite "PL 11110"}
+                       {:osoiteTyyppi "posti"
+                        :kieli "kieli_sv#1"
+                        :postinumeroUri "posti_00076"
+                        :postitoimipaikka "AALTO"
+                        :osoite "PB 11110"}]]
+      (is (= {:osoite {:fi "PL 11110" :sv "PB 11110"}
+              :postinumeroKoodiUri "posti_00076"}
+             (oppilaitos/create-kielistetty-osoitetieto postiosoite languages)))))
+  )
+
+(deftest parse-yhteystiedot
+  (testing "returns kielistetty nimi as it is in organisaatiopalveluresponse"
+    (is (= {:fi "Aalto-yliopisto"
+            :sv "Aalto-universitetet"
+            :en "Aalto University"}
+           (:nimi (nth (oppilaitos/parse-yhteystiedot oppilaitos-response languages) 0)))))
+
+  (testing "returns sahkoposti map with fi, en and sv language emails"
+    (is (= {:fi "hakijapalvelut@aalto.fi"
+            :en "admissions@aalto.fi"
+            :sv "ansokningsservice@aalto.fi"}
+           (:sahkoposti (nth (oppilaitos/parse-yhteystiedot oppilaitos-response languages) 0)))))
+
+  (testing "returns puhelinnumero map with fi, sv and en language phone numbers"
+    (is (= {:fi "0294429290"
+            :sv "0294429290"
+            :en "+358294429290"}
+           (:puhelinnumero (nth (oppilaitos/parse-yhteystiedot oppilaitos-response languages) 0)))))
+
+  (testing "returns postiosoite map with osoite and postinumero fields for all languages"
+    (is (= {:osoite {:fi "PL 11110" :sv "PB 11110"}
+            :postinumeroKoodiUri "posti_00076"}
+           (:postiosoite (nth (oppilaitos/parse-yhteystiedot oppilaitos-response languages) 0)))))
+
+  (testing "returns postiosoite map with osoite and postinumero fields for all languages"
+    (is (= {:osoite {:fi "PL 11110" :sv "PB 11110"}
+            :postinumeroKoodiUri "posti_00076"}
+           (:postiosoite (nth (oppilaitos/parse-yhteystiedot oppilaitos-response languages) 0)))))
+
+  (testing "returns kayntiosoite map with osoite and postinumero fields for all languages"
+    (is (= {:osoite {:fi "Otakaari 1" :sv "Otsvängen 1"}
+            :postinumeroKoodiUri "posti_02150"}
+           (:kayntiosoite (nth (oppilaitos/parse-yhteystiedot oppilaitos-response languages) 0)))))
+  )
+
+(require '[clojure.test :refer [run-tests]])
+(run-tests)
