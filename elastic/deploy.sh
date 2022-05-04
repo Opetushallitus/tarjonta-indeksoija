5204933ed776f365923265742fbcfb100cf02327
Petteri Tolonen <petteri.tolonen@knowit.fi>
2022-05-04 12:32:21 +0300

    Siirretty elasticsearch kontin luonti ja tägäys CI:n build-steppiin

--- elastic/deploy.sh
+++ elastic/deploy.sh
@@ -6,6 +6,5 @@ if [[ -z "$CI" ]]
 then
     aws ecr get-login-password --region eu-west-1 --profile oph-utility | docker login --username AWS --password-stdin 190073735177.dkr.ecr.eu-west-1.amazonaws.com
 fi
-docker tag elasticsearch-kouta 190073735177.dkr.ecr.eu-west-1.amazonaws.com/utility/elasticsearch-kouta:7.17.2
 docker push 190073735177.dkr.ecr.eu-west-1.amazonaws.com/utility/elasticsearch-kouta:7.17.2
 cd -
