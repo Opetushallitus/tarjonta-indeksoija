d3ad966ee8fcfec193b108576c860cb257919cca
Petteri Tolonen <petteri.tolonen@knowit.fi>
2022-09-05 11:41:56 +0300

    Korjataan kouta/konfo-lokalisointien tallennus jatkumaan, vaikka lokalisointi-palvelu palauttaisi 500 joillekin käännöksille.

--- src/kouta_indeksoija_service/lokalisointi/service.clj
+++ src/kouta_indeksoija_service/lokalisointi/service.clj
@@ -1,7 +1,6 @@
 (ns kouta-indeksoija-service.lokalisointi.service
   (:require [kouta-indeksoija-service.rest.lokalisointi :as lokalisointi-service]
-            [kouta-indeksoija-service.lokalisointi.util :as util]
-            [clojure.string :refer [split]]))
+            [kouta-indeksoija-service.lokalisointi.util :as util]))
 
 (defn ->translation-keys
   [json]
@@ -14,7 +13,8 @@
 (defn save-translation-keys-to-localisation-service
   [category lng key-value-pairs]
   (doseq [[k v] key-value-pairs]
-    (lokalisointi-service/post category lng (name k) v)))
+    ; Lokalisointi-palvelu palauttaa 500, kun yritetään päivittää jo olemassaolevaa lokalisointia ilman force-optiota
+    (try (lokalisointi-service/post category lng (name k) v) (catch Exception _))))
 
 (defn save-translation-json-to-localisation-service
   [category lng json]
