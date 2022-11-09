c830f2199b2510309f6a5fe3178be28499a4f98b
Mikko Siukola <mikko.siukola@cybercom.com>
2022-11-09 15:49:58 +0200

    OY-3965 Fetch pistehistoria from kouta and index it to hakukohde metadata

--- src/kouta_indeksoija_service/indexer/kouta/hakukohde.clj
+++ src/kouta_indeksoija_service/indexer/kouta/hakukohde.clj
@@ -346,6 +346,11 @@
     hakukohde
     (assoc hakukohde :jarjestaaUrheilijanAmmKoulutusta juak?)))
 
+(defn- assoc-pistehistoria [hakukohde pistehistoria]
+  (if (nil? pistehistoria)
+    hakukohde
+    (assoc-in hakukohde [:metadata :pistehistoria] pistehistoria)))
+
 (defn- assoc-odw-kk-tasot
   [hakukohde haku koulutus]
   (if (korkeakoulutus? koulutus)
@@ -368,7 +373,8 @@
 
 (defn create-index-entry
   [oid execution-id]
-  (let [hakukohde-from-kouta (kouta-backend/get-hakukohde-with-cache oid execution-id)]
+  (let [hakukohde-from-kouta (kouta-backend/get-hakukohde-with-cache oid execution-id)
+        hakukohdeKoodiUri (:hakukohdeKoodiUri hakukohde-from-kouta)]
     (if (not-poistettu? hakukohde-from-kouta)
       (let [hakukohde (-> hakukohde-from-kouta
                           (assoc-nimi-as-esitysnimi)
@@ -381,7 +387,14 @@
             sora-kuvaus (kouta-backend/get-sorakuvaus-with-cache (:sorakuvausId koulutus) execution-id)
             valintaperusteId (:valintaperusteId hakukohde)
             valintaperuste (when-not (str/blank? valintaperusteId)
-                             (kouta-backend/get-valintaperuste-with-cache valintaperusteId execution-id))]
+                             (kouta-backend/get-valintaperuste-with-cache valintaperusteId execution-id))
+            jarjestyspaikkaOid (get-in hakukohde [:jarjestyspaikka :oid])
+            jarjestyspaikka-oppilaitos (when-not (str/blank? jarjestyspaikkaOid)
+                                         (first
+                                           (:oppilaitokset
+                                             (kouta-backend/get-oppilaitokset-with-cache [jarjestyspaikkaOid] execution-id))))
+            pistehistoria (when (some? hakukohdeKoodiUri)
+                            (kouta-backend/get-pistehistoria-with-cache jarjestyspaikkaOid hakukohdeKoodiUri execution-id))]
         (indexable/->index-entry-with-forwarded-data oid
                                                      (-> hakukohde
                                                          (assoc-koulutustyyppi-path koulutus (:metadata toteutus))
@@ -398,6 +411,7 @@
                                                          (assoc-paatelty-alkamiskausi-for-hakukohde hakukohde-from-kouta haku toteutus)
                                                          (assoc-odw-kk-tasot haku koulutus)
                                                          (assoc-jarjestaa-urheilijan-ammatillista-koulutusta (get-in hakukohde [:metadata :jarjestaaUrheilijanAmmKoulutusta]))
+                                                         (assoc-pistehistoria pistehistoria)
                                                          (dissoc :_enrichedData)
                                                          (common/localize-dates)) hakukohde))
       (indexable/->delete-entry-with-forwarded-data oid hakukohde-from-kouta))))
