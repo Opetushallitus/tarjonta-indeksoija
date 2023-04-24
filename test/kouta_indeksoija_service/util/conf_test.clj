15790231790ac0561bca9a5cbe27ed40c35c9285
August Kilponen <august.kilponen@gmail.com>
2023-04-24 14:36:57 +0300

    Korjattu testejä

--- test/kouta_indeksoija_service/util/conf_test.clj
+++ test/kouta_indeksoija_service/util/conf_test.clj
@@ -1,23 +1,27 @@
 (ns kouta-indeksoija-service.util.conf-test
   (:require [clojure.test :refer :all]))
 
+(defn- sorted-keys
+      [map]
+      (sort (keys map)))
+
 (deftest conf-test
   (let [template-conf (read-string (slurp "./oph-configuration/config.edn.template"))]
 
     (testing "Test that ci configuration is correct"
           (let [ci-conf-template  (read-string (slurp "./ci_resources/config.edn.template"))
                 ci-conf           (read-string (slurp "./ci_resources/config.edn"))]
-            (is (= (keys template-conf)    (keys ci-conf-template)))
-            (is (= (keys ci-conf-template) (keys ci-conf)))))
+            (is (= (sorted-keys template-conf)    (sorted-keys ci-conf-template)))
+            (is (= (sorted-keys ci-conf-template) (sorted-keys ci-conf)))))
 
     (testing "Test that dev configuration is correct"
           (let [test-conf-template  (read-string (slurp "./dev_resources/config.edn.template"))]
-            (is (= (keys template-conf) (keys test-conf-template)))
+            (is (= (sorted-keys template-conf) (sorted-keys test-conf-template)))
             (when-let [test-conf (try (read-string (slurp "./dev_resources/config.edn")) (catch Exception e nil))]
-              (is (= (keys test-conf-template) (keys test-conf))))))
+              (is (= (sorted-keys test-conf-template) (sorted-keys test-conf))))))
 
     (testing "Test that test configuration is correct"
       (let [test-conf-template  (read-string (slurp "./test_resources/config.edn.template"))]
-        (is (= (keys template-conf) (keys test-conf-template)))
+        (is (= (sorted-keys template-conf) (sorted-keys test-conf-template)))
         (when-let [test-conf (try (read-string (slurp "./test_resources/config.edn")) (catch Exception e nil))]
-          (is (= (keys test-conf-template) (keys test-conf))))))))
+          (is (= (sorted-keys test-conf-template) (sorted-keys test-conf))))))))
