e5c54c37e55a9552b022a477149cf000a21295b8
Petteri Tolonen <petteri.tolonen@knowit.fi>
2023-10-16 10:38:37 +0300

    Siivotaan vanhat elasticsearch-dumpit pois ennen kuin luodaan uudet

--- tools/dump_elastic_data.sh
+++ tools/dump_elastic_data.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+rm $1/*.json
+
 docker run --add-host=host.docker.internal:host-gateway --rm -v $1:/tmp elasticdump/elasticsearch-dump \
 multielasticdump --input=$2 --output=/tmp --includeType=data,mapping,alias,settings,template
