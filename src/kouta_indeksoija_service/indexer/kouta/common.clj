e496acae54db89eac75c634d6683470f027841d4
Heidi Lammi-Mihaljov <heidi.lammi-mihaljov@gofore.com>
2024-05-22 08:40:13 +0300

    OK-472: Ei indeksoida osaamismerkkiä koodiurina (decorate-koodiuris)

--- src/kouta_indeksoija_service/indexer/kouta/common.clj
+++ src/kouta_indeksoija_service/indexer/kouta/common.clj
@@ -37,7 +37,8 @@
                       :arvo true
                       :fi true
                       :sv true
-                      :en true})
+                      :en true
+                      :osaamismerkki true})
 
 (defn- processable-as-koodi-uri? [v]
   (boolean (or (and (string? v) (koodi-uri? v))
