96a9c29e63da616e69836ec73f6e04f2f2d9fa57
Petteri Tolonen <petteri.tolonen@knowit.fi>
2024-07-30 10:25:48 +0300

    OY-4873: Elasticdump: exportoidaan myös analyzerit

--- tools/dump_elastic_data.sh
+++ tools/dump_elastic_data.sh
@@ -3,4 +3,4 @@
 rm $1/*.json
 
 docker run --add-host=host.docker.internal:host-gateway --rm -v $1:/tmp elasticdump/elasticsearch-dump \
-multielasticdump --input=$2 --output=/tmp --includeType=data,mapping,alias,settings,template
+multielasticdump --input=$2 --output=/tmp --includeType=data,mapping,alias,analyzer,settings,template
